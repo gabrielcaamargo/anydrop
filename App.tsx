6281283006e4f5e3472917f3f8cf1d4468efc89e
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-06-11 00:22:54 -0300

    feat: setup routing

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import {Routes} from '@routes';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from '@theme';
 import {Text, View} from 'react-native';
@@ -5,15 +6,7 @@ import {Text, View} from 'react-native';
 export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<View
-				style={{
-					flex: 1,
-					backgroundColor: theme.colors.backgroundColor,
-					justifyContent: 'center',
-					alignItems: 'center',
-				}}>
-				<Text style={{color: theme.colors.textPrimary}}>App.tsx</Text>
-			</View>
+			<Routes />
 		</ThemeProvider>
 	);
 }
---
+++ src/routes/navigationType.ts
@@ -0,0 +1,13 @@
+import {CompositeScreenProps} from '@react-navigation/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+import {AppStackParamList} from './AppStack';
+
+declare global {
+	namespace ReactNavigation {
+		interface RootParamList extends AppStackParamList {}
+	}
+}
+
+export type AppScreenProps<RouteName extends keyof AppStackParamList> =
+	NativeStackScreenProps<AppStackParamList, RouteName>;
