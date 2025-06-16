afd57cfe9d5e95344bf2d330885bbdc5574ecd27
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-06-16 00:48:01 -0300

    feat: remove log

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,7 @@
 import {Text, View} from 'react-native';
 import {Icon} from '@components';
-import {env} from '@config';
 
 export function HomeScreen() {
-	console.log('HomeScreennn', env.googleWebClientId);
-
 	return (
 		<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 			<Text>HomeScreen</Text>
